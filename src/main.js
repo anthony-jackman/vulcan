47bfb36aaa596b04e75603c33913ed596b81aa85
Anthony Jackman <anthony.jackman@serco-na.com>
2023-11-29 16:12:00 -0600

    Add functionality to handle Google Analytics (v4)
    Add automatic tracking
    
    TODO: According to research, CSP tends to fight GA
    TODO: will need to exclude some pages (POC, etc)
    
    Resolves #8"

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import './styles/sass/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtag from "vue-gtag";
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(VueGtag, {
+  config: { id: "G-YC5SEDKQL5" }
+}, router);
 app.use(createPinia())
 app.use(router)
 
