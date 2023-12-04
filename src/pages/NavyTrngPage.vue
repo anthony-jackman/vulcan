05ee163306a2a0ff10fb54eff0023cb6e7a58608
Anthony Jackman <57164950+anthony-jackman@users.noreply.github.com>
2023-12-04 16:04:59 -0600

    FIX class schedules in ammo74sched.json (#36)
    Add ending brace to ammo74 schedule file
    
    Resolves #35

--- src/pages/NavyTrngPage.vue
+++ src/pages/NavyTrngPage.vue
@@ -1,14 +1,15 @@
 <template>
   <h2 class="title has-text-centered mt-3">Navy Training</h2>
   <hr class="divider" />
-  <p class="block">Navy related classroom explosives safety training schedule is now listed on the NOSSA Restricted Web-Site: <a href='https://nossa.dc3n.navy.mil' target='_blank'>https://nossa.dc3n.navy.mil</a>. Once you have been logged into the website (CAC Card required), choose the "Programs" tab, hover your mouse over the "Explosives Safety Operations" menu item then select "Training" from the sub-menu that displays.</p>
+  <p class="block">Navy related classroom explosives safety training schedule is now listed on the NOSSA Restricted Web-Site: <a href='https://nossa.dc3n.navy.mil/nros' target='_blank'>https://nossa.dc3n.navy.mil</a>. Once you have been logged into the website (CAC Card required), choose the "Programs" tab, hover your mouse over the "Explosives Safety Operations" menu item then select "Training" from the sub-menu that displays.</p>
   <p class="block"><strong>To register for a classroom course:</strong> After navigating to the "Training" page described above, find and select the "Register for a Classroom Course" menu item in the Resources section of the page. Fill out and submit the form that displays on the next page.</p>
   <p class="block">To learn more information about Navy training sites that offer DAC courses, visit this site's <RouterLink :to="{ name: 'navytrngsites' }">Navy Training Site Information page</RouterLink>.</p>
   <p class="block"><strong>To register for a distance learning course:</strong> Select the Online Training menu item at the top of this website. The next page will display a table of available distance learning courses. Select the distance learning course title of interest from the list. You will be directed further depending on hosting location of the course you have selected.</p>
   <p class="block">The Defense Ammunition Center Course Catalog website will continue to list all scheduled classroom and distance learning course and their locations/dates.</p>
   <button class="button is-success mb-3">
-    <a href='https://nossa.dc3n.navy.mil' target='_blank'>NOSSA Restricted Site</a>
+    <a href='https://nossa.dc3n.navy.mil/nros' target='_blank'>NOSSA Restricted Site</a>
   </button>
-  <p class="block"><strong>Additional Information:</strong> For additional information regarding Navy training, contact:</p>
-  <p class="block">Commercial (240) 435-0444<br /><a href='mailto:larry.darnell.civ@us.navy.mil'>larry.darnell.civ@us.navy.mil</a></p>
+  <p class="block"><strong>Additional Information:</strong> For additional information regarding Navy training, contact: Commercial (240) 435-0444</p>
+  <!-- WAITING ON FURTHER DIRECTION FOR 'helpdesk' email -->
+  <!-- <p class="block">Commercial (240) 435-0444<br /><a href='mailto:larry.darnell.civ@us.navy.mil'>larry.darnell.civ@us.navy.mil</a></p> -->
 </template>
